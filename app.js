f610152ac9410e3d662c9c7ecc0c3681538609de
Anuj Soni <44522202+sonianuj181@users.noreply.github.com>
2025-08-08 00:29:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-res.status(202).send("PORT IS UP Yo for cicd")    
+res.status(202).send("PORT IS UP Yo for cicd version 1 ")    
 })
 app.listen('8080',()=>{
     console.log("PORT IS UP")
